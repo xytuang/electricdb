9c83045d370f83286f58f3827240e0f1b4286f9c
Xiang Yu Tuang <xiangyutuang@gmail.com>
2026-01-07 04:26:51 -0500

    fix: Update nullmask to check bounds before looking up index

--- src/execution/vector/nullmask.cpp
+++ src/execution/vector/nullmask.cpp
@@ -1,9 +1,10 @@
 #include "electricdb/execution/vector/nullmask.h"
 
+#include <cassert>
 #include <cstring>
 
 namespace electricdb {
-NullMask::NullMask(Arena &arena, uint32_t capacity) {
+NullMask::NullMask(Arena &arena, uint32_t capacity) : capacity(capacity) {
 	byte_count = (capacity + 7) / 8;
 	bits = reinterpret_cast<uint8_t *>(arena.allocate(byte_count, alignof(uint8_t)));
 
@@ -12,22 +13,31 @@ NullMask::NullMask(Arena &arena, uint32_t capacity) {
 }
 
 bool NullMask::IsNull(uint32_t idx) const noexcept {
-	/**
-	 * idx >> 3 tells us which byte we are lookng at in the array
-	 * idx & 7 gives us the lower 3 bits of the idx
-	 */
+/**
+ * idx >> 3 tells us which byte we are lookng at in the array
+ * idx & 7 gives us the lower 3 bits of the idx
+ */
+#ifndef NDEBUG
+	assert(idx < capacity);
+#endif
 	return bits[idx >> 3] & (1u << (idx & 7));
 }
 
-void NullMask::SetNull(uint32_t idx) {
+void NullMask::SetNull(uint32_t idx) noexcept {
+#ifndef NDEBUG
+	assert(idx < capacity);
+#endif
 	bits[idx >> 3] |= (1u << (idx & 7));
 }
 
-void NullMask::ClearNull(uint32_t idx) {
+void NullMask::ClearNull(uint32_t idx) noexcept {
+#ifndef NDEBUG
+	assert(idx < capacity);
+#endif
 	bits[idx >> 3] &= ~(1u << (idx & 7));
 }
 
-void NullMask::Reset() {
+void NullMask::Reset() noexcept {
 	std::memset(bits, 0, byte_count);
 }
 } // namespace electricdb
--- src/include/electricdb/execution/vector/nullmask.h
+++ src/include/electricdb/execution/vector/nullmask.h
@@ -10,12 +10,13 @@ class NullMask {
   public:
 	explicit NullMask(Arena &arena, uint32_t capacity);
 	bool IsNull(uint32_t idx) const noexcept;
-	void SetNull(uint32_t idx);
-	void ClearNull(uint32_t idx);
-	void Reset();
+	void SetNull(uint32_t idx) noexcept;
+	void ClearNull(uint32_t idx) noexcept;
+	void Reset() noexcept;
 
   private:
 	uint8_t *bits;
 	uint32_t byte_count;
+	uint32_t capacity;
 };
 } // namespace electricdb
