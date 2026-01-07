a9447a61f31525323de8a846e074b802bddc9ffe
Xiang Yu Tuang <xiangyutuang@gmail.com>
2026-01-07 05:05:21 -0500

    fix: Reduce simd header. Will be implemented later

--- src/include/electricdb/util/simd.h
+++ src/include/electricdb/util/simd.h
@@ -1,35 +1,17 @@
 #pragma once
 
 #include <cstddef>
-#include <cstdint>
 
 namespace electricdb {
 
-class Simd {
-  public:
-	// SIMD width in bytes (e.g., 16 for SSE, 32 for AVX2)
-	static size_t width();
+inline constexpr size_t kDefaultWidth = 1;
 
-	// Load/store
-	static void load(const void *src, void *dst);
-	static void store(const void *src, void *dst);
+inline constexpr size_t Width() noexcept {
+	return kDefaultWidth;
+}
 
-	// Comparisons
-	// Returns a bitmask of lanes where comparison is true
-	static uint32_t cmp_eq_i32(const int32_t *a, const int32_t *b);
-	static uint32_t cmp_lt_i32(const int32_t *a, const int32_t *b);
-	static uint32_t cmp_gt_i32(const int32_t *a, const int32_t *b);
-
-	// Mask utilities
-	static uint32_t mask_all();
-	static uint32_t mask_none();
-	static size_t popcount(uint32_t mask);
-
-	// Prefetch hint
-	static void prefetch(const void *ptr);
-
-  private:
-	Simd() = delete;
-};
+inline constexpr bool Enabled() noexcept {
+	return false;
+}
 
 } // namespace electricdb
